b07fecca4ee75be6306d65808feb098c509ba177
PJKTDELFOS <albert.franca1992@gmail.com>
2025-06-24 13:45:35 -0300

    Commit dia 22/06/2025 operacional

--- operacional/urls.py
+++ operacional/urls.py
@@ -14,27 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
-from perfil.views import menu_inicial
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
-#adicionar na url de entrada o setor
+app_name='operacional'
+
 urlpatterns = [
-    #path('rh/', include('perfil.urls')),
-    path('', include('perfil.urls')),
-    path('admin/', admin.site.urls,name='index'),
-    path('comercial', include('processos.urls')),
-    path('financeiro', include('financeiro.urls')),
-
-    path('operacional',include('operacional.urls')),
+    path('logistica/',views.operacionalteste,name='logistica'),
 
 ]
-
-
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- operacional/views.py
+++ operacional/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
+
 
 # Create your views here.
+
+
+def operacionalteste(request):
+    return HttpResponse('estou no operacional')
