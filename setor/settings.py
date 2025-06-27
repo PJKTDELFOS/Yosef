6cab49644bf2387df9b212fde15a343262d49d5d
PJKTDELFOS <albert.franca1992@gmail.com>
2025-06-27 18:23:54 -0300

    corrigindo estaticos

--- setor/settings.py
+++ setor/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'setor.urls'
@@ -143,9 +144,10 @@ STATICFILES_DIRS=(
 
     BASE_DIR/'base_static',
 )
-STATIC_ROOT=BASE_DIR/'static'# collectstatic
+STATIC_ROOT=BASE_DIR/'static'
 MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR/'media'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
